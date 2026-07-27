0c78cbd390c804d85d14cb08f58643447699c6c6
domination@2006 <sanjog.santra2006@gmail.com>
2026-07-27 20:47:18 +0530

    Added 2 spheres

--- include/vec.h
+++ include/vec.h
@@ -13,11 +13,19 @@ public:
     __host__ __device__ float x() const {return e[0];}
     __host__ __device__ float y() const {return e[1];}
     __host__ __device__ float z() const {return e[2];}
-
+    
     __host__ __device__ vec operator-() const{ return vec(-e[0], -e[1] , -e[2]); }
     __host__ __device__ float operator[](int i) const{return e[i] ;}
     __host__ __device__ float& operator[](int i) {return e[i] ;}
 
+    //  Copy Constructor
+    __host__ __device__ vec(const vec& v) : e{v.e[0] ,v.e[1] , v.e[2]}  {} ;
+
+    __host__ __device__ vec& operator=(const vec& v){  // Assignment operator
+        e[0] = v.e[0];
+        e[1] = v.e[1];
+        e[2] = v.e[2];
+    }  
     __host__ __device__ vec& operator += (const vec& v){
         e[0] += v.e[0];
         e[1] += v.e[1];
