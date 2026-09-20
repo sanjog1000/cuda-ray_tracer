41d91701337873853308f3bd8e443d887262ff34
dominator <sanjog.santra2006@gmail.com>
2026-09-20 20:30:21 +0530

    fix: stabilize renderer

--- include/vec.h
+++ include/vec.h
@@ -18,7 +18,6 @@ public:
     __host__ __device__ float operator[](int i) const{return e[i] ;}
     __host__ __device__ float& operator[](int i) {return e[i] ;}
 
-    //  Copy Constructor
     __host__ __device__ vec(const vec& v) : e{v.e[0] ,v.e[1] , v.e[2]}  {} ;
 
     __host__ __device__ vec& operator=(const vec& v){  // Assignment operator
